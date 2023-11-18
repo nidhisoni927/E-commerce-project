0665ba04555c516d0a4355b7e6c96812d8b57064
Nidhi Soni <nidhisoni927@gmail.com>
2023-11-18 13:37:54 +0530

    add seller and home

---
+++ src/app/seller-auth/seller-auth.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-seller-auth',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './seller-auth.component.html',
+  styleUrl: './seller-auth.component.css'
+})
+export class SellerAuthComponent {
+
+}
