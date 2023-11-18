34c2c58f47b4a3003b00c59bf2a9c7d79cac4765
Nidhi Soni <nidhisoni927@gmail.com>
2023-11-18 16:30:23 +0530

    create form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
-import { Routes } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {SellerAuthComponent} from "./seller-auth/seller-auth.component";
+import {NgModule} from "@angular/core";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'seller-auth',
+    component:SellerAuthComponent
+  }];
+
+@NgModule(
+  {
+    imports:[RouterModule.forRoot(routes)],
+    exports:[RouterModule]
+  })
+export class AppRoutingModule {
+
+}
