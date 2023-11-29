c7d75758bb2ee7d7c7fe307ce95aea26015967b2
Nidhi Soni <nidhisoni927@gmail.com>
2023-11-29 13:50:27 +0530

    simple form updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import {HeaderComponent} from "./header/header.component";
+
 import {HttpClient, HttpClientModule} from "@angular/common/http";
 
-// interface ICommnet
+
 export interface CommentInterface {
   id: number,
   postId?: number,
@@ -15,7 +15,7 @@ export interface CommentInterface {
 @Component({
   selector: 'app-root',
   standalone: true,
-    imports: [CommonModule, RouterOutlet, HeaderComponent, HttpClientModule],
+    imports: [CommonModule, RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -24,14 +24,9 @@ export class AppComponent {
   constructor(private http: HttpClient) {
     this.http.get("http://localhost:3000/comments").subscribe( (response)  => {
       console.log(response);
-      // response.forEach( (data) => {
-      //   console.log(data.id);
-      // });
-      // console.log(res.length);
-      // console.log(res[0].id);
+
     });
-    // this.title = 'E-commerce-project';
-    // console.log("app.component");
+
   }
 
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,11 @@
 import {RouterModule, Routes} from '@angular/router';
-import {HomeComponent} from "./home/home.component";
+
 
 import {NgModule} from "@angular/core";
 
 
 export const routes: Routes = [
-  {
-    path:'',
-    component:HomeComponent
-  },
+
   ];
 
 @NgModule(
