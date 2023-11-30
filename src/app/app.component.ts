e4968f701eefb66cc0889222a1e0900da345f051
Nidhi Soni <nidhisoni927@gmail.com>
2023-11-30 20:40:25 +0530

    interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,15 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-
-import {HttpClient, HttpClientModule} from "@angular/common/http";
-
-
-export interface CommentInterface {
-  id: number,
-  postId?: number,
-  body?: string
-}
-
 
 @Component({
   selector: 'app-root',
   standalone: true,
-    imports: [CommonModule, RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'E-commerce-project';
-  constructor(private http: HttpClient) {
-    this.http.get("http://localhost:3000/comments").subscribe( (response)  => {
-      console.log(response);
-
-    });
-
-  }
+public name="Nidhi";
+ constructor() {
+ }
 
 }
